c577197794c5b7d5aa9f39e420a2abcfb47ffedf
Jack Tudor <jack.tudor@theappbusiness.com>
2018-02-22 18:49:19 +0000

    Pulling out some of the question view objects into their own classes

---
+++ MusicQuizApp/QuizGameSection/AnswerButton.swift
@@ -0,0 +1,9 @@
+//
+//  AnswerButton.swift
+//  MusicQuizApp
+//
+//  Created by John Tudor on 22/02/2018.
+//  Copyright © 2018 Jack Tudor. All rights reserved.
+//
+
+import Foundation
---
+++ MusicQuizApp/QuizGameSection/CountdownView.swift
@@ -0,0 +1,9 @@
+//
+//  CountdownView.swift
+//  MusicQuizApp
+//
+//  Created by John Tudor on 22/02/2018.
+//  Copyright © 2018 Jack Tudor. All rights reserved.
+//
+
+import Foundation
